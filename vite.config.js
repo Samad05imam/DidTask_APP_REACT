0093b16e56f7f083e63505070092dec76fde2005
Samad05imam <samad1419imam@gmail.com>
2025-05-04 03:17:41 +0530

    fix: allow Render domain in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: ['didtask-app-react-1.onrender.com'],
+  },
   plugins: [react() , tailwindcss()],
 })
